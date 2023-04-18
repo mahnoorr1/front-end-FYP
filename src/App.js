0d7639dd0fc49a43a31a0b849dfb8cc8c79fe14c
Hamza Shahid <hs623965@gmail.com>
2023-04-18 20:32:07 +0500

    Fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './globe.png';
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom'
+import {  Routes, Route } from 'react-router-dom'
 
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -15,89 +15,15 @@ import './App.css';
 function App() {
 
   return(
-    <Routes>
-      <Route path='/' component={<SignIn />}/>
-      <Route path='/signup' component={SignUp}/>
-    </Routes>
+    <>
+      <Routes>
+      <Route path="/" element={<SignIn />} />
+
+        <Route path='/signup' element={<SignUp />}/>
+      </Routes>
+    </>
   )
 
-  // const [email, setEmail] = useState('');
-  // const [password, setPassword] = useState('');
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={require('./globe.png')} className="App-logo"/>
-  //       <div className='background'>
-  //         <div id='background'></div>
-  //         <div className='sign'>Sign In</div>
-  //         <hr></hr>
-  //         <div >
-  //           <form className='input_column'>
-  //             <TextField
-  //             className='field'
-  //             InputLabelProps={{
-  //               style: { color: 'rgb(174, 193, 209)', }, 
-  //            }}
-  //               label="Email"
-  //               type="email"
-  //               required
-  //               value={email}
-  //               onChange={e => setEmail(e.target.value)}
-  //             />
-  //             <div className='break'></div>
-              
-  //             <TextField
-  //             className='field'
-  //             InputLabelProps={{
-  //               style: { color: 'rgb(174, 193, 209)', }, 
-  //            }}
-  //               label="Password"
-  //               type="password"
-  //               required
-  //               value={password}
-  //               onChange={e => setPassword(e.target.value)}
-  //             />
-  //             <FormControlLabel
-  //             control={<Checkbox value="remember" sx = {{color: 'white'}}/>}
-  //             label="Remember me"
-  //             className = "remember"
-  //           />
-  //           <Button 
-            
-  //             style={{
-  //               borderRadius: 10,
-  //               padding: "10px 26px",
-  //               fontSize: "16px",
-  //           }}
-  //             type="submit"
-  //             fullWidth
-  //             variant="outlined"
-  //             sx={{ mt: 2, mb: 2, color: 'white'}}
-  //           >
-  //             Sign In
-  //           </Button>
-  //           <Grid container>
-  //             <Grid item xs>
-  //               <Link href="#" variant="body2" color={"rgb(216, 232, 245)"}>
-  //                 Forgot password?
-  //               </Link>
-  //             </Grid>
-  //             <Grid item>
-  //               <Link href="#" variant="body2" color={"rgb(216, 232, 245)"}>
-  //                 {"Don't have an account? Sign Up"}
-  //               </Link>
-  //             </Grid>
-  //           </Grid>
-  //           </form>
-              
-  //         </div>
-        
-  //       </div>
-        
-        
-  //     </header>
-  //   </div>
-  // );
 }
 
 export default App;
